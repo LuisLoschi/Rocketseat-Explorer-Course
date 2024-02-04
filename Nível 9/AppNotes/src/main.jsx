264747292aeddd3884c267facdb25b611a51a532
LuisLoschi <gustavoloschi362@gmail.com>
2024-02-03 22:24:39 -0300

    Global style

--- Nível 9/AppNotes/src/main.jsx
+++ Nível 9/AppNotes/src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+//Styles
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyle from './styles/global'
+
 import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
