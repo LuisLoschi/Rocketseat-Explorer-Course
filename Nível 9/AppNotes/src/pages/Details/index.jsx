7e63dab3cfc111717d8e84be9134e5663971d85d
LuisLoschi <gustavoloschi362@gmail.com>
2024-02-15 23:40:07 -0300

    First page

--- Nível 9/AppNotes/src/pages/Details/index.jsx
+++ Nível 9/AppNotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
@@ -12,21 +12,36 @@ export function Details() {
         <Container>
             <Header />
 
-            <ButtonText title="Excluir nota" />
+            <main>
+                <Content>
 
-            <Section title="Links úteis">
-                <Links>
-                    <li><a href="https://github.com/LuisLoschi" target="_blank">https://github.com/LuisLoschi</a></li>
-                    <li><a href="#">https://github.com/LuisLoschi</a></li>
-                </Links>
-            </Section>
+                    <ButtonText title="Excluir nota" />
 
-            <Section title="Marcadores">
-                <Tag title="express"/>
-                <Tag title="node js"/>
-            </Section>
+                    <h1>
+                        Introdução ao React
+                    </h1>
 
-            <Button title="Voltar"/>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi nisi inventore laborum nam et laudantium ab eum mollitia repudiandae, 
+                        praesentium, magni consectetur libero temporibus eos eligendi in minima a omnis!
+                    </p>
+
+                    <Section title="Links úteis">
+                        <Links>
+                            <li><a href="https://github.com/LuisLoschi" target="_blank">https://github.com/LuisLoschi</a></li>
+                            <li><a href="#">https://github.com/LuisLoschi</a></li>
+                        </Links>
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <Tag title="express"/>
+                        <Tag title="node js"/>
+                    </Section>
+
+                    <Button title="Voltar"/>
+
+                </Content>
+            </main>
         </Container>
     )
 }
