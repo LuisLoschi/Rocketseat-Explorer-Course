2e5e971c45ac4c949a43de655b22ac2e6b9a3436
LuisLoschi <gustavoloschi362@gmail.com>
2024-02-18 17:06:08 -0300

    Page Profile

--- Nível 9/AppNotes/src/main.jsx
+++ Nível 9/AppNotes/src/main.jsx
@@ -10,6 +10,7 @@ import { Home } from './pages/Home'
 import { Details } from './pages/Details'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 
 
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-        <SignUp />
+        <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ Nível 9/AppNotes/src/pages/Profile/index.jsx
@@ -0,0 +1,67 @@
+import { Container, Form, Avatar } from "./styles";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { FiLock, FiMail, FiUser, FiArrowLeft, FiCamera } from "react-icons/fi";
+
+
+export function Profile() {
+
+    return (
+        <Container>
+            <header>
+                <a href="/">
+                    <FiArrowLeft />
+                </a>
+            </header>
+            
+            <Form >
+                <Avatar >
+                    <img 
+                        src="https://github.com/LuisLoschi.png" 
+                        alt="Foto do usuário" 
+                    />
+
+                    <label htmlFor="avatar">
+                        <FiCamera />
+
+                        <input 
+                            type="file" 
+                            id="avatar" 
+                        />
+                    </label>
+                </Avatar>
+                
+                <Input 
+                    placeholder="Nome"  
+                    type="text" 
+                    icon={FiUser}
+                />
+
+                <Input 
+                    placeholder="E-mail" 
+                    type="email" 
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placeholder="Senha atual"  
+                    type="password" 
+                    icon={FiLock}
+                />
+
+                <Input 
+                    placeholder="Nova senha"  
+                    type="password" 
+                    icon={FiLock}
+                />
+
+                <Button title="Salvar" />
+
+
+            </Form>
+
+        </Container>
+
+    )
+}
