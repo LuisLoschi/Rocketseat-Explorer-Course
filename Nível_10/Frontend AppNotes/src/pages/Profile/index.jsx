e637d86ed5684661a6871d99be5e914949abb810
LuisLoschi <gustavoloschi362@gmail.com>
2024-04-11 23:28:19 -0300

    new: upload profile data

--- Nível_10/Frontend AppNotes/src/pages/Profile/index.jsx
+++ Nível_10/Frontend AppNotes/src/pages/Profile/index.jsx
@@ -5,8 +5,16 @@ import { Button } from "../../components/Button";
 import { FiLock, FiMail, FiUser, FiArrowLeft, FiCamera } from "react-icons/fi";
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../../hook/auth";
 
 export function Profile() {
+    const { user } = useAuth();
+
+    const [name, setName] = useState(user.name);
+    const [email, setEmail] = useState(user.email);
+    const [passwordOld, setPasswordOld] = useState();
+    const [passwordNew, setPasswordNew] = useState();
 
     return (
         <Container>
@@ -37,24 +45,30 @@ export function Profile() {
                     placeholder="Nome"  
                     type="text" 
                     icon={FiUser}
+                    value={name}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input 
                     placeholder="E-mail" 
                     type="email" 
                     icon={FiMail}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input 
                     placeholder="Senha atual"  
                     type="password" 
                     icon={FiLock}
+                    onChange={e => setPasswordOld(e.target.value)}
                 />
 
                 <Input 
                     placeholder="Nova senha"  
                     type="password" 
                     icon={FiLock}
+                    onChange={e => setPasswordNew(e.target.value)}
                 />
 
                 <Button title="Salvar" />
